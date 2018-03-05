8c5726a2baacbb7a27e18686816bd2fa93bf5529
neoa <acrixf@gmail.com>
2018-03-05 17:41:27 -0600

    Context

---
+++ Chapter-02/ClosureContext.js
@@ -0,0 +1,22 @@
+function setTestValue( value ) {
+    var firstNum = value || 2;
+
+    console.log("F: " + firstNum );
+
+    return function ( secondNum ) {
+
+        console.log( "S: " + secondNum );
+
+        if( firstNum > secondNum ) {
+            return firstNum;
+        } else if (firstNum < secondNum ){
+            return secondNum;
+        } else {
+            return "";
+        }
+    };
+}
+
+var theNumberExaminer = setTestValue( 6 );
+var result = theNumberExaminer( 2 );
+console.log( result );
